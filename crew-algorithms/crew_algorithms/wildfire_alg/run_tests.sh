48df38da55bba380ca296c0258930ff8565c5a58
Paul Parayil Varkey <paulvarkey@meta.com>
2025-07-16 16:38:36 -0700

    small fixes for MacOS builds

--- crew-algorithms/crew_algorithms/wildfire_alg/run_tests.sh
+++ crew-algorithms/crew_algorithms/wildfire_alg/run_tests.sh
@@ -1,4 +1,4 @@
-python algorithms\\CAMON\\__main__.py envs.level=Cut_Trees_Sparse_small envs.seed=483  envs.max_steps=20
+python algorithms/CAMON/__main__.py envs.level=Cut_Trees_Sparse_small envs.seed=483  envs.max_steps=20
 
 wait
-echo "All tests finished"  
+echo "All tests finished"
